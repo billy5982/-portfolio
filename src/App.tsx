5aad24a8236c093929b55e606ef653a915358d33
billy5982 <kuu0404@naver.com>
2023-06-16 17:14:28 +0900

    ⚙️ setting : 절대 경로 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import vite from '@/assets/vite.svg';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -7,6 +7,7 @@ function App() {
     <div className="min-h-full flex justify-center items-center">
       <h1 className="text-3xl font-bold text-blue-600">
         Install & Setup Vite + React + Typescript + Tailwind CSS 3
+        <img src={vite} />
       </h1>
     </div>
   );
