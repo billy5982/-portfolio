da771fc5fe84cb2efeb943f20445e802eaf4c0c9
billy5982 <kuu0404@naver.com>
2023-06-18 00:48:01 +0900

    ⚙️ setting : BMTI, 기타 이미지 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
-import Banggyul from '@/assets/Banggyul.png';
+import Banggyul from '@/assets/projectSpriteImg/Banggyul.png';
+import BMTI from '@/assets/projectSpriteImg/BMTI.png';
+import react from '@/assets/stackIcon/React.svg';
+import banggyulLogo from '@/assets/captionLogo/banggyul-logo.svg';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="min-h-full flex justify-center items-center">
+    <div className="flex justify-center items-center bg-black">
       <h1 className="text-3xl font-bold text-blue-600">
         Install & Setup Vite + React + Typescript + Tailwind CSS 3
         <img src={Banggyul} />
+        <img src={react} />
+        <img src={banggyulLogo} />
+        <img src={BMTI} />
       </h1>
     </div>
   );
