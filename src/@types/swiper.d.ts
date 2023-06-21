3cf1f5329ad922b5c36b8cc7d14f082102fc2bd7
billy5982 <kuu0404@naver.com>
2023-06-21 19:26:58 +0900

    ⚙️ setting : swiperRef 세팅

--- src/@types/swiper.d.ts
+++ src/@types/swiper.d.ts
@@ -1,1 +1,9 @@
 declare module 'swiper';
+
+interface SwiperRef extends React.HTMLAttributes<HTMLElement> {
+  swiper: SwiperClass;
+}
+
+interface SwiperRef extends React.HTMLAttributes<HTMLElement> {
+  swiper: SwiperClass;
+}
