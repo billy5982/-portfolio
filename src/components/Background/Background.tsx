9b21668e363f8a27ba0c20b55e1fa4307d8e7556
billy5982 <kuu0404@naver.com>
2023-06-24 07:17:11 +0900

    🎨 style :Background 제거

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -4,14 +4,14 @@ import * as S from '@/components/Background/Background.style';
 const Background = () => {
   return (
     <S.SolarSyst starnum={500} maxarea={1800}>
-      <div className="sun" />
+      {/* <div className="sun" />
       <div className="mercury" />
       <div className="venus" />
       <div className="earth" />
       <div className="mars" />
       <div className="jupiter" />
       <div className="saturn" />
-      <div className="asteroids-belt" />
+      <div className="asteroids-belt" /> */}
     </S.SolarSyst>
   );
 };
