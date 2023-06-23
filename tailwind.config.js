378f47e38de20acac36208ebb7345905ab1d13fd
billy5982 <kuu0404@naver.com>
2023-06-23 11:58:59 +0900

    🐛 fix :  오타 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         primary: '#f8f8f8',
         highlight: '#79E1AB',
         desc: '#91DD78',
-        logoBg: '6919ff',
+        logoBg: '#6919ff',
         caption1: '#F7C95F',
         caption2: '#91DD78',
       },
